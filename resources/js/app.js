ab21ebf0832357deec86d201af95f8e6faf14e20
yohannmarchand <marchandyohannnnn@gmail.com>
2021-07-05 13:37:10 +0200

    test qr code

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,12 @@ require('./bootstrap');
 
 
 require('@fortawesome/fontawesome-free/js/all.js');
+
+import QRScanner from 'qr-code-scanner';
+
+QRScanner.initiate({
+    match: /^[a-zA-Z0-9]{16,18}$/, // optional
+    onResult: function (result) { console.info('DONE: ', result); },
+    onError: function (err) { console.error('ERR :::: ', err); }, // optional
+    onTimeout: function () { console.warn('TIMEOUT'); } // optional
+})
