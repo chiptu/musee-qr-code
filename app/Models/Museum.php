fcfaa073d01d8ea50bf488ecb41b8b161da8ce10
yohannmarchand <marchandyohannnnn@gmail.com>
2021-07-08 10:18:09 +0200

    resolve logo image bug

--- app/Models/Museum.php
+++ app/Models/Museum.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
-//use Intervention\Image\ImageManagerStatic as Image;
-
+use Intervention\Image\ImageManagerStatic as Image;
 
 use App\Models\User;
 use App\Models\Artwork;
@@ -23,8 +22,6 @@ class Museum extends Model
     protected $fillable = [
         'name','adress', 'logo', 'description', 'qrCodeSize'
     ];
-
-
 
     protected $casts = [
         'adress' => 'json'
@@ -43,9 +40,41 @@ class Museum extends Model
     public function setLogoAttribute($value)
     {
         $attribute_name = "logo";
-        $disk = "public";
-        $destination_path="/museum/logo";
+        // or use your own disk, defined in config/filesystems.php
+        $disk = 'public';
+        // destination path relative to the disk above
+        $destination_path = "/museum/logo";
 
-        $this->uploadFileToDisk($value, $attribute_name, $disk, $destination_path);
+        // if the image was erased
+        if ($value==null) {
+            // delete the image from disk
+            \Storage::disk($disk)->delete($this->{$attribute_name});
+
+            // set null in the database column
+            $this->attributes[$attribute_name] = null;
+        }
+
+        // if a base64 was sent, store it in the db
+        if (Str::startsWith($value, 'data:image'))
+        {
+            // 0. Make the image
+            $image = \Image::make($value)->encode('jpg', 90);
+
+            // 1. Generate a filename.
+            $filename = md5($value.time()).'.jpg';
+
+            // 2. Store the image on disk.
+            \Storage::disk($disk)->put($destination_path.'/'.$filename, $image->stream());
+
+            // 3. Delete the previous image, if there was one.
+            \Storage::disk($disk)->delete($this->{$attribute_name});
+
+            // 4. Save the public path to the database
+            // but first, remove "public/" from the path, since we're pointing to it
+            // from the root folder; that way, what gets saved in the db
+            // is the public URL (everything that comes after the domain name)
+            $public_destination_path = Str::replaceFirst('public/', '', $destination_path);
+            $this->attributes[$attribute_name] = 'storage'.$public_destination_path.'/'.$filename;
+        }
     }
 }
